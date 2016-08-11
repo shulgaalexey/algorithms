e9a3c19f7bc74fdec51c0d07e3f720dfdacdefe5
shulga <a.shulga@samsung.com>
2016-08-11 16:10:17 +0900

    added matrix chain problem

--- dp.cpp
+++ dp.cpp
@@ -4,6 +4,7 @@
 //   - bottom up approach
 //   - memoization
 // 2. Fibonacci calculation with O(N)
+// 3. Matrix Chain Product problem
 //
 
 #include <iostream>
@@ -72,39 +73,97 @@ int fibonacci(int n) {
 	return ret;
 }
 
+void print_parens(int *s, const int i, const int j, int length) {
+	if(i == j)
+		cout << "A" << i;
+	else {
+		cout << "(";
+		print_parens(s, i, s[i * length + j], length);
+		print_parens(s, s[i * length +  j] + 1, j, length);
+		cout << ")";
+	}
+}
 
+// Matrix Chain Product problem
+int matrix_chain(int *p, const int length) { // length is a size of p, including p[0]
+	const int n = length - 1;
+	int m[length][length];
+	int s[length][length];
+
+	// Filling the main diagonal with zeros
+	// because it indicates the product of matrix to itself
+	// and its cost is zero
+	for(int i = 1; i <= n; i ++) // l is 1: cost is 0
+		m[i][i] = 0;
+
+	// Filling other diagonals top to the main diagonal
+	for(int l = 2; l <= n; l ++) { // l is a length of sequence of matrixes in the product
+		for(int i = 1; i <= (n - l + 1); i ++ ) { // i is starting matrix in a sequence
+			// we should try all matrices except the last on in the sequence
+
+			const int j = i + l -1; // index of the last matrix in the sequence
+
+			m[i][j] = 1000000; // pre-init, +inf
+
+			for(int k = i; k <= (j - 1); k ++) { // trying all split points
+				const int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+				if(q < m[i][j]) {
+					m[i][j] = q;
+					s[i][j] = k;
+				}
+			}
+		}
+	}
+
+	// Reconstructing the solution
+	print_parens((int *)s, 1, n, length);
+	cout << endl;
+
+	return m[1][n];
+}
 
 int main(void) {
-	int p[] = {0, 1, 5, 8, 10, 13, 17, 17, 20, 24, 30};
-	const int n = int(sizeof(p) / sizeof(p[0]));
-
-	int check[] = {0, 1, 5, 8, 10, 13, 17, 18, 22, 25, 30};
-	for(int i = 0; i < (n - 1); i ++) {
-		const int ret1 = dp_bottom_up(p, i);
-		if(ret1 != check[i])
-			cout << "Error Bottom Up: Expected " << check[i] << " obtained " << ret1 << endl;
-		const int ret2 = dp_memoized(p, i, NULL);
-		if(ret2 != check[i])
-			cout << "Error Memoized: Expected " << check[i] << " obtained " << ret2 << endl;
+	{ // Matrix chain problem
+		int p[] = {30, 35, 15, 5, 10, 20, 25};
+		const int ret = matrix_chain(p, int(sizeof(p) / sizeof(p[0])));
+		if(ret != 15125)
+			cout << "Error Matrix Chain: " << ret << endl;
+		else
+			cout << ret << " OK" << endl;
 	}
 
-	{ // return the actual solution
-		const int l = 9;
-		int *history = new int[l + 1];
-		dp_memoized(p, l, history);
-		int rod_remainder = l;
-		while(rod_remainder) {
-			cout << "cut " << history[rod_remainder] << endl;
-			rod_remainder -= history[rod_remainder];
+	{ // Rod cutting problem
+		int p[] = {0, 1, 5, 8, 10, 13, 17, 17, 20, 24, 30};
+		const int n = int(sizeof(p) / sizeof(p[0]));
+
+		int check[] = {0, 1, 5, 8, 10, 13, 17, 18, 22, 25, 30};
+		for(int i = 0; i < (n - 1); i ++) {
+			const int ret1 = dp_bottom_up(p, i);
+			if(ret1 != check[i])
+				cout << "Error Bottom Up: Expected " << check[i] << " obtained " << ret1 << endl;
+			const int ret2 = dp_memoized(p, i, NULL);
+			if(ret2 != check[i])
+				cout << "Error Memoized: Expected " << check[i] << " obtained " << ret2 << endl;
 		}
 
-		delete [] history;
-	}
+		{ // return the actual solution
+			const int l = 9;
+			int *history = new int[l + 1];
+			dp_memoized(p, l, history);
+			int rod_remainder = l;
+			while(rod_remainder) {
+				cout << "cut " << history[rod_remainder] << endl;
+				rod_remainder -= history[rod_remainder];
+			}
 
-	{ // Fibonacci
-		cout << endl << "Fibonacci numbers:" << endl;
-		for(int i = 0; i < 10; i ++)
-			cout << i << ": " << fibonacci(i) << endl;
+			delete [] history;
+		}
+
+		{ // Fibonacci
+			cout << endl << "Fibonacci numbers:" << endl;
+			for(int i = 0; i < 10; i ++)
+				cout << i << ": " << fibonacci(i) << endl;
+		}
 	}
 
 	return 0;
