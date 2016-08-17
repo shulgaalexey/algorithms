d76cfaaee25a25ecbe0551f25c54e85fc2685f7c
shulga <a.shulga@samsung.com>
2016-08-17 09:47:23 +0900

    Added reconstruction of BST

--- dp_optimal_bst.cpp
+++ dp_optimal_bst.cpp
@@ -1,7 +1,19 @@
 // Implementation of Optimal Binary Search Tree
 // using Dynamic Programmin
 #include <iostream>
+#include <vector>
 using namespace std;
+
+vector<vector<int> > levels(6);
+
+void reconstr(int *root, int i, int j, int n, int level) {
+	if((j < i) || (i > j))
+		return;
+	const int r = root[i * n + j];
+	levels[level].push_back(r);
+	reconstr(root, i, r - 1, n, level + 1);
+	reconstr(root, r + 1, j, n, level + 1);
+}
 
 double optimal_bst(const double *p, const double *q, const int n) {
 	double e[n + 2][n + 1] = {0};
@@ -27,6 +39,24 @@ double optimal_bst(const double *p, const double *q, const int n) {
 			}
 		}
 	}
+
+	// reconstructing the tree
+	/*for(int i = 0; i <= n; i ++) {
+		for(int j = 0; j <= n; j ++) {
+			cout << root[i][j] << " ";
+		}
+		cout << endl;
+	}*/
+
+	reconstr((int *)root, 1, n, n + 1, 0);
+	for(size_t i = 0; i < levels.size(); i ++) {
+		for(size_t j = 0; j < levels[i].size(); j ++) {
+			cout << levels[i][j] << " ";
+		}
+		if(!levels[i].empty())
+			cout << endl;
+	}
+
 	return e[1][n];
 }
 
