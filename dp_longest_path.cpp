ed5970f66bafbb9360ff9814949e0b80b59f2399
shulga <a.shulga@samsung.com>
2016-09-02 08:47:07 +0900

    Fixed Longest Path problem solution

--- dp_longest_path.cpp
+++ dp_longest_path.cpp
@@ -6,46 +6,87 @@
 #include <iostream>
 using namespace std;
 
-#define INF -100000.
+#define INF -1
 
-double find_longest_path(const double *E, const int n) {
+//
+// P[i,j] = max<P[i,j], P[i,l] + P[l,j]>
+//
+// DP: processing diagonals of the matrix, one-by-one
+//
+int find_longest_path(const int *E, const int n) {
 	double P[n][n] = {0}; // table with paths calculated
 
-	for(int i = 0; i < n; i ++)
-		P[i][i] = 0;
 
+	// initialization
+	for(int i = 0; i < n; i ++)
+		for(int j = 0; j < n; j ++)
+			P[i][j] = E[i * n + j];
+
+	/*cout << endl;
 	for(int i = 0; i < n; i ++) {
 		for(int j = 0; j < n; j ++) {
-			if(i == j)
-				continue;
-			for(int r = 0; r < n; r ++) {
-				if(r == j)
-					P[i][j] = max(P[i][j], E[i * n + j]);
-				else if(r != i) {
-					const double tmp = P[i][r] + E[r * n + j];
-					P[i][j] = max(P[i][j], tmp);
+			if(P[i][j] < 0)
+				cout << "I ";
+			else
+				cout << P[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;*/
+
+	// DP
+	// Upper triangle of the matrix
+	for(int j = 2; j < n; j ++) {
+		for(int i = 0; i < j; i ++) {
+			for(int l = (i + 1); l < j; l ++) {
+				if((P[i][j - 1] != INF) && (P[i + 1][j] != INF)) {
+					P[i][j] = max(P[i][j], P[i][j - 1] + P[i + 1][j]);
 				}
 			}
 		}
 
-		for(int j = 0; j < n; j ++)
-			cout << P[i][j] << " ";
+	}
+
+
+	// DP
+	// Lower triangle of the matrix
+	for(int i = 2; i < n; i ++) {
+		for(int j = 0; j < (i - 1); j ++) {
+			for(int l = (j + 1); l < j; l ++) {
+				if((P[i - 1][j] != INF) && (P[i][j + 1] != INF)) {
+					P[i][j] = max(P[i][j], P[i - 1][j] + P[i][j + 1]);
+				}
+			}
+		}
+	}
+
+	/*for(int i = 0; i < n; i ++) {
+		for(int j = 0; j < n; j ++) {
+			if(P[i][j] < 0)
+				cout << "I ";
+			else
+				cout << P[i][j] << " ";
+		}
 		cout << endl;
 	}
+	cout << endl;*/
+
 	return P[0][n - 1];
 }
 
 int main(void) {
-	const double E[5][5] = {
-		0,   1,   3,   5,   7,
+	const int E[5][5] = {
+		0,   1,   3,   5,   8,
 		INF, 0,   8,   INF, 2,
-		INF, INF, 0,   9,   4,
-		INF, INF, INF, 0,   6
+		INF, INF, 0,   INF, 4,
+		INF, INF, 9,   0,   7
 		INF, INF, INF, INF, 0
 	};
 
-	const double p = find_longest_path((const double *)E, 5);
+	const int p = find_longest_path((const int *)E, 5);
 	cout << p << endl;
+	if(p != 8)
+		cout << "ERROR" << endl;
 	return 0;
 }
 
