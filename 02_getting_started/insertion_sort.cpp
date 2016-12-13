e97ebd59be665e778f0073ce9ce02edb558413ec
shulga <a.shulga@samsung.com>
2016-12-13 09:05:33 +0900

    Added decreasing order sort

--- 02_getting_started/insertion_sort.cpp
+++ 02_getting_started/insertion_sort.cpp
@@ -16,27 +16,64 @@ void sort(int *a, const int n) {
 	}
 }
 
+void sort_decr(int *a, const int n) {
+	for(int j = 1; j < n; j ++) {
+		int key = a[j];
+		int i = j - 1;
+		while((i >= 0) && (a[i] < key)) {
+			a[i + 1] = a[i];
+			i --;
+		}
+		a[i + 1] = key;
+	}
+}
+
 int main(void) {
 
 	srand(time(NULL));
 
-	const int n = 15;
-	int *a = new int[n];
+	{ // Increasing order
+		cout << "Increasing order sort" << endl;
+		const int n = 15;
+		int *a = new int[n];
 
-	for(int i = 0; i < n ; i ++)
-		a[i] = rand() % 10;
+		for(int i = 0; i < n ; i ++)
+			a[i] = rand() % 10;
 
-	for(int i = 0; i < n; i ++)
-		cout << a[i] << " ";
-	cout << endl;
+		for(int i = 0; i < n; i ++)
+			cout << a[i] << " ";
+		cout << endl;
 
-	sort(a, n);
+		sort(a, n);
 
-	for(int i = 0; i < n; i ++)
-		cout << a[i] << " ";
-	cout << endl;
+		for(int i = 0; i < n; i ++)
+			cout << a[i] << " ";
+		cout << endl;
 
-	delete [] a;
+		delete [] a;
+	}
+
+	{ // Decreasing order
+		cout << "Decreasing order sort" << endl;
+		const int n = 15;
+		int *a = new int[n];
+
+		for(int i = 0; i < n ; i ++)
+			a[i] = rand() % 10;
+
+		for(int i = 0; i < n; i ++)
+			cout << a[i] << " ";
+		cout << endl;
+
+		sort_decr(a, n);
+
+		for(int i = 0; i < n; i ++)
+			cout << a[i] << " ";
+		cout << endl;
+
+		delete [] a;
+	}
+
 
 	return 0;
 }
